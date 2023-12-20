55adf6c65543c15a8bba8786b2bd3e614478ac3a
Pratiksha-NS <149141209+Pratiksha-NS@users.noreply.github.com>
2023-12-20 10:57:29 +0530

    Update CreateRecipe.js

--- client/src/pages/CreateRecipe.js
+++ client/src/pages/CreateRecipe.js
@@ -38,7 +38,7 @@ export default function CreateRecipe() {
     event.preventDefault();
 
     try {
-      await axios.post("http://localhost:5000/recipes", recipe , 
+      await axios.post("https://savorsync-backend.onrender.com/recipes", recipe , 
       { headers: {authorization: cookies.access_token}});
       alert("Recipe Created!");
       navigate("/");
